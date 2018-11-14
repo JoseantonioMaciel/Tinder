0a8bd9ae9b65335c0028bfc063514372cc27a6ef
tonyomendoza <mendozatony@outlook.com>
2018-11-13 23:55:09 -0800

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Tony Mendoza on 11/13/18.
+//  Copyright © 2018 jmaciel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
